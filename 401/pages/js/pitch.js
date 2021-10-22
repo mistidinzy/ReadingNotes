9e117d951329de92b0ca24d383b394e36e77a690
mistidinzy <mistidinzy@gmail.com>
2021-10-22 12:42:58 -0500

    dark mode toggle button

--- 401/pages/js/pitch.js
+++ 401/pages/js/pitch.js
@@ -1,6 +1,15 @@
 
 
 function myFunction() {
+
+  var lightSwitch = document.getElementById("lightSwitch");
+  
+  if(lightSwitch.value === "🌙")
+    {
+      lightSwitch.value = "🔆";
+    } else lightSwitch.value = "🌙";
+
   var element = document.body;
   element.classList.toggle("dark-mode");
+
 }
