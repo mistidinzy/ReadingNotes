9f387d44dbab232268e9b139df47f7e3c490da27
mistidinzy <mistidinzy@gmail.com>
2021-11-07 22:44:05 -0600

    playing with react

---
+++ React/readingnotes/src/js/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default class App extends React.Component {
+ 
+  render() {
+    return (
+      <>
+        <Router>
+          <Header onLogout={this.logoutHandler} />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    )
+  }
+}
